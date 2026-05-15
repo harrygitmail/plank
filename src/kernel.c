cdaab041d99b68e9db33a341a5de7270a385ace8
harry <harry.git.mail@gmail.com>
2026-05-15 19:54:35 +0530

    solved path resolve bug in get_kernel_list()

--- src/kernel.c
+++ src/kernel.c
@@ -4,6 +4,7 @@
 #include <sys/types.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #include <unistd.h>
 #include "common.h"
 #include "types.h"
@@ -14,7 +15,7 @@ enum plank_status get_kernel_list(kernel_list *const ret, const char *root)
 	int root_fd = open(root, O_RDONLY);
 	if(root_fd < 0 ) return PLANK_ERR;
 
-	char *kernel_lib = "/lib/modules";
+	char *kernel_lib = "lib/modules";
 	size_t counts = 0;
 	char **list = list_files(kernel_lib, root_fd, &counts);
 
@@ -43,12 +44,13 @@ kernel_list *kernel_diff(const kernel_list *k1, const kernel_list *k2)
 	for (size_t i = 0; i < k1->counts; i++) {
 		int found = 0;
 
+		char *comp_1;
+		comp_1 = k1->list[i].kernel_ver;
+
 		for (size_t j = 0; j < k2->counts; j++) {
 
-			char *comp_1;
 			char *comp_2;
 
-			comp_1 = k1->list[i].kernel_ver;
 			comp_2 = k2->list[j].kernel_ver;
 
 			int c = strcmp(comp_1, comp_2);
@@ -61,7 +63,8 @@ kernel_list *kernel_diff(const kernel_list *k1, const kernel_list *k2)
 
 		if (found == 0) {
 
-			strcpy(list[diff_c++].kernel_ver, k1->list[i].kernel_ver);
+			strcpy(list[diff_c].kernel_ver, k1->list[i].kernel_ver);
+			++diff_c;
 
 		}
 	}
